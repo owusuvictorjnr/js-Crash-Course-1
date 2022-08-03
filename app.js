85531c23f3174140e8a474542011023cf1d527b0
owusuvictorjnr <owusuvictor80@gmail.com>
2022-08-03 14:20:44 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+// Declaring a variable
+// Keyword identifier/variable = value
+
+
+// var firstName = "Vitech";
+// let lastName = "Solution";
+// let age = 10;
+// // const fullName = firstName + " " + lastName; //concatenation
+// let children = ["Bright", "Simon", "Patrick"];
+// let other =  {
+//     favoriteColor: "Gold",
+//     favoriteSport : "Soccer",
+//     favoriteFruit: "Apple"
+// }
+
+// Object
+let person = {
+    firstName: "Vitech",
+    lastName: "Solutions",
+    age: 23,
+};
+
+console.log(person['firstName']);
+console.log(person.age)
+console.log(Object.keys(person))
+console.log(Object.values(person))
+console.log(Object.entries(person))
+
+// const mySelf = `My name is ${firstName} ${lastName}`  //string interpolation
+
+// // string
+// let sentence = "This is is the story about Vitech Solutions"
+
+
+// console.log(mySelf, children, other, sentence.length);
